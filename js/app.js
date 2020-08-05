bcbfe1cae4f34d3cd1ab172b1df0f480929f12c3
Czarl Jalos <czarlfrancis@gmail.com>
2020-08-05 09:36:23 -0700

    Add else if statement.

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ alert('Hi ' + fullName + '!');
 var wantsToPlayAGame = prompt('do you want to play a game 5 question game?');
 if (wantsToPlayAGame.toLocaleLowerCase() === 'yes' || wantsToPlayAGame.toLocaleLowerCase() === 'y') {
   alert('Awesome ' + fullName + '! answer yes or no to guess. :)');
-} else {
+} else if (wantsToPlayAGame.toLocaleLowerCase() === 'no' || wantsToPlayAGame.toLocaleLowerCase() === 'n') {
   alert('Hope to see you agian ' + fullName + '!');
   }
 
@@ -15,7 +15,7 @@ var myLast = prompt('My last name is Jalos, yes or no?');
 if (myLast.toLocaleLowerCase() === 'yes' || myLast.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
   alert('You\' correct ' + fullName +'! Next Question.' );
-} else {
+} else if (myLast.toLocaleLowerCase() === 'no' || myLast.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time.') ;
   }
 
@@ -23,7 +23,7 @@ var myFirst = prompt('Is my First name Czarl?');
 if (myFirst.toLocaleLowerCase() === 'yes' || myFirst.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
   alert('You\' correct ' + fullName +'! Next Question.');
-} else {
+} else if (myFirst.toLocaleLowerCase() === 'no' || myFirst.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time.');
   }
 
@@ -31,7 +31,7 @@ var vet = prompt('Am i an Air force veteran?');
 if (vet.toLocaleLowerCase() === 'no' || vet.toLocaleLowerCase() === 'n') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
   alert('You\' correct ' + fullName +'! I\'m a Navy veteran. Next Question.' );
-} else {
+} else if (vet.toLocaleLowerCase() === 'yes' || vet.toLocaleLowerCase() === 'y') {
   alert('I\'m a Navy veteran guess better next time.')
   }
 
@@ -39,7 +39,7 @@ var lang = prompt('Am i a Code fellow student?')
 if (lang.toLocaleLowerCase() === 'yes' || lang.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
   alert('You\' correct ' + fullName +'! Check out my Biography.' );
-} else {
+} else if (lang.toLocaleLowerCase() === 'no' || lang.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time and check out my Biography.');
   }
 
