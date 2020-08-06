eeabe94a52a0ccacd726adbab7cb4e76b0c2209f
Czarl Jalos <czarlfrancis@gmail.com>
2020-08-05 19:10:47 -0700

    Added code for 7th question.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
+var gradeCounter = 0;
+
 var fullName = prompt('What is your full name?')
 console.log('Full name is ' + fullName);
-alert('Hi ' + fullName + '!');
+alert('Hi ' + fullName + ' Let"s play a game!');
 
 var wantsToPlayAGame = prompt('do you want to play a game 5 question game?');
 if (wantsToPlayAGame.toLocaleLowerCase() === 'yes' || wantsToPlayAGame.toLocaleLowerCase() === 'y') {
@@ -14,6 +16,7 @@ if (wantsToPlayAGame.toLocaleLowerCase() === 'yes' || wantsToPlayAGame.toLocaleL
 var myLast = prompt('My last name is Jalos, yes or no?');
 if (myLast.toLocaleLowerCase() === 'yes' || myLast.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
+  gradeCounter += 1;
   alert('You\' correct ' + fullName +'! Next Question.' );
 } else if (myLast.toLocaleLowerCase() === 'no' || myLast.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time.') ;
@@ -22,6 +25,7 @@ if (myLast.toLocaleLowerCase() === 'yes' || myLast.toLocaleLowerCase() === 'y') 
 var myFirst = prompt('Is my First name Czarl?');
 if (myFirst.toLocaleLowerCase() === 'yes' || myFirst.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
+  gradeCounter += 1;
   alert('You\' correct ' + fullName +'! Next Question.');
 } else if (myFirst.toLocaleLowerCase() === 'no' || myFirst.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time.');
@@ -30,20 +34,31 @@ if (myFirst.toLocaleLowerCase() === 'yes' || myFirst.toLocaleLowerCase() === 'y'
 var vet = prompt('Am i an Air force veteran?');
 if (vet.toLocaleLowerCase() === 'no' || vet.toLocaleLowerCase() === 'n') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
+  gradeCounter += 1;
   alert('You\' correct ' + fullName +'! I\'m a Navy veteran. Next Question.' );
 } else if (vet.toLocaleLowerCase() === 'yes' || vet.toLocaleLowerCase() === 'y') {
   alert('I\'m a Navy veteran guess better next time.')
   }
 
+var lang = prompt('Is Hotdog a Sandwich?')
+if (lang.toLocaleLowerCase() === 'yes' || lang.toLocaleLowerCase() === 'y') {
+  //console.log('You\' correct ' + fullName +'! Next Question.');
+  gradeCounter += 1;
+  alert('You\' correct ' + fullName +'!.' );
+} else if (lang.toLocaleLowerCase() === 'no' || lang.toLocaleLowerCase() === 'n') {
+  alert('Wrong! guess better next time. :)');
+  }
+
 var lang = prompt('Am i a Code fellow student?')
 if (lang.toLocaleLowerCase() === 'yes' || lang.toLocaleLowerCase() === 'y') {
   //console.log('You\' correct ' + fullName +'! Next Question.');
+  gradeCounter += 1;
   alert('You\' correct ' + fullName +'! Check out my Biography.' );
 } else if (lang.toLocaleLowerCase() === 'no' || lang.toLocaleLowerCase() === 'n') {
   alert('Wrong! guess better next time.');
   }
 
-  alert('Thanks for playing yes or no' + fullName + '. Wanna play guess a number?');
+  alert('Thanks for playing yes or no' + fullName + '. Lets play guess a number!');
   
   // This is the code for the 6th Question
   alert(' GUESS THE NUMBER!');
@@ -56,12 +71,14 @@ for (var i = 1; i <= chances; i++) {
   var answer = '5';
   var index = i;
   if (randomNum === answer) {
+    gradeCounter += 1;
     alert('Proceed to next level!');
     break;
   } else if (index === 3 && randomNum < answer) {
     alert('Still low! You\'re almost there! LAST CHANCE!');
     randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10."); 
       if (randomNum === answer) {
+        gradeCounter += 1;
       alert('Correct! Proceed to next level!');
       break;
       } else if (randomNum !== answer){
@@ -71,7 +88,8 @@ for (var i = 1; i <= chances; i++) {
       alert('Still High! You\'re almost there! LAST CHANCE!');
       randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10."); 
       if (randomNum === answer) {
-        alert('Correct! Proceed to next level!');
+        gradeCounter += 1;
+        alert('Correct! Proceed to next Question!');
         break;
     } else if (randomNum !== answer){
         alert('You used up all your chances. Better luck next time!')
@@ -85,6 +103,45 @@ for (var i = 1; i <= chances; i++) {
     } 
 }
     
+// this is the 7th question game
+
+alert(' GUESS MY TOP COUNTRY!');
+alert('You have 6 chances to get it right!')
+
+var randomAns;
+var limiter;
+var answers = ['japan','australia','thailand','brazil','chile','singapore'];
+
+for ( var i = 0; i < answers.length; i++) { 
   
+var endOfIndex = answers[answers.length - 1]; 
+  
+  if (limiter) {
+    alert('Congratulations! you guess one of the answers. here"s all the possible answers. ' + answers);
+    break;
+  }
+  if (answers[i] === endOfIndex) {
+    alert('last chance to guess!!')
+  }
+  randomAns = prompt('Guess a country!')
+  for ( var j = 0 ; j < answers.length; j++ ) {
+    var lowCase = randomAns.toLowerCase(); 
+    if (lowCase === answers[j]) {
+      limiter += '0'
+      gradeCounter += 1;
+      alert('Good Job!');
+      break;
+    } else if (answers[j] === endOfIndex && lowCase !== answers[j]) {
+      alert('Try again, Sorry!');
+      break;
+    }
+  }
+ if (answers[i] === endOfIndex && limiter === undefined )  {
+   alert('You used up all your chances! review the possible answers ' + answers);
+ }
+  
+}
+
+alert('Out of 7 question you answered ' + gradeCounter + ' correct answer!');
 
 
