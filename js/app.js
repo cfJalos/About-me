96e180b0555b4fa68e6c46a83b278dc62e319de0
CHRISTOPHER QUIOCHO <CHRISTOPHER.QUIOCHO@GMAIL.COM>
2020-08-06 13:54:58 -0700

    Functions added to JS

--- js/app.js
+++ js/app.js
@@ -6,64 +6,29 @@ var fullName = prompt('What is your full name?')
 console.log('Full name is ' + fullName);
 alert('Hi ' + fullName + ' Let"s play a game!');
 
-var wantsToPlayAGame = prompt('do you want to play a game 5 question game?');
-if (wantsToPlayAGame.toLocaleLowerCase() === 'yes' || wantsToPlayAGame.toLocaleLowerCase() === 'y') {
-  alert('Awesome ' + fullName + '! answer yes or no to guess. :)');
-} else if (wantsToPlayAGame.toLocaleLowerCase() === 'no' || wantsToPlayAGame.toLocaleLowerCase() === 'n') {
-  alert('Hope to see you agian ' + fullName + '!');
-  }
+function question(string){
+  var random = prompt(string);
+  if (random.toLocaleLowerCase() === 'yes' || random.toLocaleLowerCase() === 'y') {
+    alert('You\'re correct ' + fullName +'! Next Question.' );
+  } else if (random.toLocaleLowerCase() === 'no' || random.toLocaleLowerCase() === 'n') {
+    alert('Wrong! guess better next time.') ;
+    }
+    return random;
+}
+question('My last name is Jalos, yes or no?');
+question('Is my First name Czarl?');
+question('Am i an Air force veteran?');
+question('Is Hotdog a Sandwich?');
 
-var myLast = prompt('My last name is Jalos, yes or no?');
-if (myLast.toLocaleLowerCase() === 'yes' || myLast.toLocaleLowerCase() === 'y') {
-  //console.log('correct ' + fullName +'! Next Question.');
-  gradeCounter += 1;
-  alert('You\'re correct ' + fullName +'! Next Question.' );
-} else if (myLast.toLocaleLowerCase() === 'no' || myLast.toLocaleLowerCase() === 'n') {
-  alert('Wrong! guess better next time.') ;
-  }
-
-var myFirst = prompt('Is my First name Czarl?');
-if (myFirst.toLocaleLowerCase() === 'yes' || myFirst.toLocaleLowerCase() === 'y') {
-  //console.log('correct ' + fullName +'! Next Question.');
-  gradeCounter += 1;
-  alert('correct ' + fullName +'! Next Question.');
-} else if (myFirst.toLocaleLowerCase() === 'no' || myFirst.toLocaleLowerCase() === 'n') {
-  alert('Wrong! guess better next time.');
-  }
-
-var vet = prompt('Am i an Air force veteran?');
-if (vet.toLocaleLowerCase() === 'no' || vet.toLocaleLowerCase() === 'n') {
-  //console.log('correct ' + fullName +'! Next Question.');
-  gradeCounter += 1;
-  alert('correct ' + fullName +'! I\'m a Navy veteran. Next Question.' );
-} else if (vet.toLocaleLowerCase() === 'yes' || vet.toLocaleLowerCase() === 'y') {
-  alert('I\'m a Navy veteran guess better next time.')
-  }
-
-var lang = prompt('Is Hotdog a Sandwich?')
-if (lang.toLocaleLowerCase() === 'yes' || lang.toLocaleLowerCase() === 'y') {
-  //console.log('correct ' + fullName +'! Next Question.');
-  gradeCounter += 1;
-  alert('correct ' + fullName +'!.' );
-} else if (lang.toLocaleLowerCase() === 'no' || lang.toLocaleLowerCase() === 'n') {
-  alert('Wrong! guess better next time. :)');
-  }
-
-var lang = prompt('Am i a Code fellow student?')
-if (lang.toLocaleLowerCase() === 'yes' || lang.toLocaleLowerCase() === 'y') {
-  //console.log('correct ' + fullName +'! Next Question.');
-  gradeCounter += 1;
-  alert('correct ' + fullName +'! Check out my Biography.' );
-} else if (lang.toLocaleLowerCase() === 'no' || lang.toLocaleLowerCase() === 'n') {
-  alert('Wrong! guess better next time.');
-  }
-
-  alert('Thanks for playing yes or no ' + fullName + '. Lets play guess a number!');
+alert('Thanks for playing yes or no ' + fullName + '. Lets play guess a number!');
   
 // This is the code for the 6th Question
 
 alert(' GUESS THE NUMBER!');
-  
+
+function guess(){
+
+
 var randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10. YOU HAVE 4 CHANCES TO GET THE RIGHT ANSWER");
 
 var chances = 3;
@@ -106,7 +71,10 @@ for (var i = 1; i <= chances; i++) {
       randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10.")
     } 
 }
-    
+}
+guess();
+
+function country(){
 // this is the 7th question game
 
 alert(' GUESS MY TOP COUNTRY!');
@@ -146,6 +114,8 @@ var endOfIndex = answers[answers.length - 1];
  }
   
 }
+}
+country();
 
 alert('Out of 7 question you answered ' + gradeCounter + ' correct answer!');
 
