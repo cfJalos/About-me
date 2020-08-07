31efb48a36c35a139a120077cccd256e6550e655
Czarl Jalos <czarlfrancis@gmail.com>
2020-08-06 23:12:46 -0700

    Added array of answers to each question.

--- js/app.js
+++ js/app.js
@@ -6,19 +6,31 @@ var fullName = prompt('What is your full name?')
 console.log('Full name is ' + fullName);
 alert('Hi ' + fullName + ' Let"s play a game!');
 
+var qOneAnswer = ['yes','y','no','n'];
+var qSixAnswer = ['5'];
+var qSevenAnswers = ['japan','australia','thailand','brazil','chile','singapore'];
+
 function question(string){
   var random = prompt(string);
-  if (random.toLocaleLowerCase() === 'yes' || random.toLocaleLowerCase() === 'y') {
+  for (var i = 0; i < qOneAnswer.length; i++)
+  if (random.toLocaleLowerCase() === qOneAnswer[0] || random.toLocaleLowerCase() === qOneAnswer[1]) {
     alert('You\'re correct ' + fullName +'! Next Question.' );
-  } else if (random.toLocaleLowerCase() === 'no' || random.toLocaleLowerCase() === 'n') {
+    break;
+  } else if (random.toLocaleLowerCase() === qOneAnswer[2] || random.toLocaleLowerCase() === qOneAnswer[3]) {
     alert('Wrong! guess better next time.') ;
+    break;
+    } else {
+      alert('Please answer yes or no/ y or n');
+      random = prompt(string);
     }
     return random;
 }
+
 question('My last name is Jalos, yes or no?');
 question('Is my First name Czarl?');
-question('Am i an Air force veteran?');
+question('Am i an Navy veteran?');
 question('Is Hotdog a Sandwich?');
+question('Am i an Student at Codefellows?');
 
 alert('Thanks for playing yes or no ' + fullName + '. Lets play guess a number!');
   
@@ -26,94 +38,70 @@ alert('Thanks for playing yes or no ' + fullName + '. Lets play guess a number!'
 
 alert(' GUESS THE NUMBER!');
 
+
 function guess(){
 
-
-var randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10. YOU HAVE 4 CHANCES TO GET THE RIGHT ANSWER");
-
-var chances = 3;
-
+var chances = 4;
+  
 for (var i = 1; i <= chances; i++) {
-  var answer = '5';
-  var index = i;
-  if (randomNum === answer) {
+ var randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10.");
+ var index = i;
+  if (randomNum === qSixAnswer) {
     gradeCounter += 1;
-    alert('Proceed to next level!');
+    alert('Correct!Proceed to next question!');
     //console.log('Correct! Proceed to next Question!');
     break;
-  } else if (index === 3 && randomNum < answer) {
-    alert('Still low! You\'re almost there! LAST CHANCE!');
-    randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10."); 
-      if (randomNum === answer) {
-        gradeCounter += 1;
-      alert('Correct! Proceed to next level!');
-      //console.log('Correct! Proceed to next level!')
-      break;
-      } else if (randomNum !== answer){
-      alert('You used up all your chances. Better luck next time!')
-      }  
-  } else if (index === 3 && randomNum > answer) {
-      alert('Still High! You\'re almost there! LAST CHANCE!');
-      randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10."); 
-      if (randomNum === answer) {
-        gradeCounter += 1;
-        alert('Correct! Proceed to next Question!');
-        //console.log('Correct! Proceed to next Question!');
-        break;
-    } else if (randomNum !== answer){
-        alert('You used up all your chances. Better luck next time!')
-        }  
-  } else if (randomNum < answer) {
+  } else if (index === 4 && randomNum !== qSixAnswer) {
+    alert('You used up all your chances. Better luck next time!')
+  } else if (randomNum < qSixAnswer) {
       alert('too low! try again!');
-      randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10.")
-    } else if (randomNum > answer) {
+      
+  } else if (randomNum > qSixAnswer) {
       alert('too high! try again!');
-      randomNum = prompt("PICK A NUMBER BETWEEN 1 AND 10.")
-    } 
+  } 
+ }
 }
-}
+
 guess();
 
-function country(){
 // this is the 7th question game
 
 alert(' GUESS MY TOP COUNTRY!');
 alert('You have 6 chances to get it right!')
 
-var randomAns;
-var limiter;
-var answers = ['japan','australia','thailand','brazil','chile','singapore'];
+function country() {
+  var randomAns;
+  var limiter;
 
-for ( var i = 0; i < answers.length; i++) { 
+  for ( var i = 1; i <= 6; i++) { 
   
-var endOfIndex = answers[answers.length - 1]; 
-  
-  if (limiter) {
-    alert('Congratulations! you guess one of the answers. here"s all the possible answers. ' + answers);
-    break;
-  }
-  if (answers[i] === endOfIndex) {
-    alert('last chance to guess!!')
-  }
-  randomAns = prompt('Guess a country!')
-  for ( var j = 0 ; j < answers.length; j++ ) {
-    var lowCase = randomAns.toLowerCase(); 
-    if (lowCase === answers[j]) {
-      limiter += '0'
-      gradeCounter += 1;
-      alert('Good Job!');
-      //console.log('Correct!')
-      break;
-    } else if (answers[j] === endOfIndex && lowCase !== answers[j]) {
-      alert('Try again, Sorry!');
+    if (limiter) {
+      alert('Congratulations! you guess one of the answers. here"s all the possible answers. ' + answers);
       break;
     }
+    if (i === 6) {
+      alert('Last chance to guess!!')
+    }
+    randomAns = prompt('Guess my top Country!')
+
+    for ( var j = 0 ; j < qSevenAnswers.length; j++ ) {
+      var lowCase = randomAns.toLowerCase(); 
+      if (lowCase === qSevenAnswers[j]) {
+        limiter += '0'
+        // gradeCounter += 1;
+        alert('Good Job! you got it right! Check out my biography');
+        //console.log('Correct!')
+        break;
+      } else if (qSevenAnswers[j] === qSevenAnswers[qSevenAnswers.length-1] && lowCase !== qSevenAnswers[j]) {
+        alert('Try again, Sorry!');
+        break;
+      }
+    }
+   if (i === 6 && limiter === undefined )  {
+     alert('You used up all your chances! review the possible answers ' + qSevenAnswers);
+   }
+
   }
- if (answers[i] === endOfIndex && limiter === undefined )  {
-   alert('You used up all your chances! review the possible answers ' + answers);
- }
-  
-}
 }
 country();
 
